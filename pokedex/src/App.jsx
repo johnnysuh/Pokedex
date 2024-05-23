7dc50fb6640fe868cdf70c7905210ed12b7ab274
johnnysuh <nicolas.c.duarte@estudante.sesisenai.org.br>
2024-05-23 11:50:50 -0300

    pokedex

---
+++ pokedex/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Pokedex from './components/Pokedex'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Logo from './components/Logo'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Logo/>
+    <Pokedex/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
