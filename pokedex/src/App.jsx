ceac76a428b5b83caf61168c25e06b55548804d5
johnnysuh <nicolas.c.duarte@estudante.sesisenai.org.br>
2024-06-06 12:14:31 -0300

    pokedex-animada

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -10,8 +10,8 @@ import styled from 'styled-components'
 const Spinner = styled(motion.div)`
   width: 50px;
   height: 50px;
-  border: 4px solid black;
-  border-top: 4px solid red;
+  border: 4px solid white;
+  border-top: 4px solid rgb(219, 33, 61);
   border-radius: 50%;
 `
 
@@ -30,7 +30,7 @@ export default function App() {
       {Carregando ? (
       <Spinner
       animate={{rotate: 360}}
-      transition={{duration: 0.2, repeat: Infinity, ease: 'linear'}}
+      transition={{duration: 0.5, repeat: Infinity, ease: 'linear'}}
       />
       ):(
         <div> 
